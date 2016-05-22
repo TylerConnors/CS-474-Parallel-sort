fba575710740f0a08af91f7ed9829fbf39b9905a
brindmo <brindmo@gmail.com>
2016-05-22 14:55:50 -0700

    Add the array and a method for filling it

--- Program.cs
+++ Program.cs
@@ -4,13 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Parallel_Sorting_Project
+namespace ParallelSort
 {
     class Program
     {
+        public static int SIZE = 100;
+        public static int[] randArray = new int[SIZE];
+
+        // Fill an array with random numbers
+        public static int[] fillArray(int[] inArray)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < inArray.Length; i++)
+            {
+                inArray[i] = rand.Next();
+            }
+
+            return inArray;
+        }
+
+        // Sort an array
+        public static int[] sortArray(int[] inArray)
+        {
+            return inArray;
+        }
+
+        // Sort an array using a parallel approach
+        public static int[] sortArrayParallel(int[] inArray)
+        {
+            return inArray;
+        }
+
         static void Main(string[] args)
         {
-
+            fillArray(randArray);
         }
     }
 }
