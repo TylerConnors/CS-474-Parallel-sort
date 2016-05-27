71faa493c50969e8e0083edca5351e01de22afa1
brindmo <brindmo@gmail.com>
2016-05-27 16:37:30 -0700

    Added an incomplete parallel function, insertion sort

--- Program.cs
+++ Program.cs
@@ -32,6 +32,74 @@ namespace Quicksorts
             return inArray;
         }
 
+        public static int[] partition(int[] inArray, int pv)
+        {
+            int n = inArray.Length;
+
+            Parallel.For(0, processorCount, ii =>
+            {
+                double chunkSize = Math.Ceiling((double)(n / processorCount));
+                double chunkStart = chunkSize * ii;
+                double chunkEnd = chunkSize * (ii + 1);
+                if (chunkEnd > n)
+                {
+                    chunkEnd = n;
+                }
+
+                double lessEqualPivot = chunkStart;
+                double morePivot = chunkEnd - 1;
+
+                for (int j = (int)chunkStart; j > chunkEnd; j++)
+                {
+                    if (inArray[ii] <= pv)
+                    {
+                        // TODOTODOTODOTODO
+                        lessEqualPivot++;
+                    }
+                    else
+                    {
+                        // TODOTODOTODOTODO
+                        morePivot++;
+                    }
+                }
+
+            });
+            return null; //FIX THIS!
+        }
+
+        public static void parallelSort(int[] inArray)
+        {
+            partitions.Push(inArray);
+            while (partitions.Count != 0)
+            {
+                int[] temp = partition((int[])partitions.Pop(), inArray[0]);
+                if (temp.Length > 10)
+                {
+                    partitions.Push(temp);
+                }
+                else
+                {
+                    chunks.Push(temp);
+                }
+            }
+        }
+
+        // Insertion sort function
+        public static int[] insertionSort(int[] inArray, int left, int right)
+        {
+            int temp;
+            for (int i = left; i < right; i++)
+            {
+                for (int j = i; j > 0 && inArray[j] < inArray[j - 1]; j--)
+                {
+                    temp = inArray[j];
+                    inArray[j] = inArray[j - 1];
+                    inArray[j - 1] = temp;
+                }
+            }
+            return inArray;
+        }
+
         // Entry function for the quicksort
         public static void quicksort(int[] inArray)
         {
