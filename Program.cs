9463577bdf1aa80a7d5fa63ce9d7dac4448886a6
TylerConnors <tconnors14@mail.wou.edu>
2016-05-29 23:18:49 -0700

    all else works but parallel is buggy
    initialized the arrays in main as well as size so we can manually select size. it crashes at the parallel sort and i don't know why.

--- Program.cs
+++ Program.cs
@@ -4,26 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel;
+using System.Diagnostics;  // for the stop watch
+
 namespace Quicksorts
 {
     class Program
     {
-        public static int SIZE = 100;
-        public static int[] randArray = new int[SIZE];
+        public static int SIZE;
+        public static int[] randArray;
         public static int processorCount = Environment.ProcessorCount;
         public static Stack partitions = new Stack();
         public static Stack chunks = new Stack();
-        public static int[] randArray2 = new int[SIZE];
-        public static int[] randArray3 = new int[SIZE];
+        public static int[] randArray2;
+        public static int[] randArray3;
+        
 
-        // Sequentially prints the contents of an array
+        // Sequentially prints the contents of an array up to the first 500 
         public static void printArray(int[] inArray)
         {
-            for (int i = 0; i < inArray.Length; i++)
+            int temp = 500;
+
+            if(inArray.Length < temp)
+            {
+                temp = inArray.Length;
+            }
+
+            for (int i = 0; i < temp; i++)
             {
                 Console.WriteLine(inArray[i]);
             }
+
+
         }
 
         // Fill an array with random numbers
@@ -32,37 +43,7 @@ namespace Quicksorts
             Random rand = new Random();
             for (int i = 0; i < inArray.Length; i++)
             {
-                inArray[i] = rand.Next(100);
-            }
-            return inArray;
-        }
-
-        // creates an array of unique numbers from 0 to n-1
-        public static int[] populateArray(int[] inArray)
-        {
-            int chunk = ((SIZE) / Environment.ProcessorCount); //ensures best speed for computer
-            Parallel.For(0, SIZE / chunk, j =>
-            {
-                int start = j * chunk;     // Inclusive start point for chunk
-                int end = (j + 1) * chunk; // Exclusive endpoint for chunk
-                for (int i = start; i < end; i++)
-                {
-                    inArray[i] = i;
-                }
-            });
-
-            //shuffles the list
-            Random rnd = new Random();
-            int temp = 0;
-            int temp2 = 0;
-            for (int i = 0; i < SIZE - 1; i++)
-            {
-                temp = rnd.Next(SIZE - 1);
-                if (i == temp)   //try to swap again if it picks the same index
-                    temp = rnd.Next(SIZE - 1);
-                temp2 = inArray[i];
-                inArray[i] = inArray[temp];
-                inArray[temp] = temp2;
+                inArray[i] = rand.Next(inArray.Length);
             }
             return inArray;
         }
@@ -147,17 +128,17 @@ namespace Quicksorts
         {
             int pivot = inArray[(startSortIndex + endSortIndex) / 2];
 
-            if (startSortIndex + endSortIndex < 15)
-            {
+           // if (startSortIndex + endSortIndex < 15)
+            //{
                 int m = partition(inArray, (endSortIndex - startSortIndex), pivot);  // the first part was shown as inArray[b] but than it would be an int, if there are errors look into this
                 int pc = ((p * (m - startSortIndex)) / (endSortIndex - startSortIndex));
                 doPSort(inArray, startSortIndex, m, pc);
                 doPSort(inArray, m, endSortIndex, p - pc);
-            }
-            else
-            {
-                insertionSort(inArray, startSortIndex, endSortIndex);
-            }
+            //}
+            //else
+            //{
+            //    insertionSort(inArray, startSortIndex, endSortIndex);
+            //}
         }
 
         public static void parallelSort(int[] inArray)
@@ -226,22 +207,48 @@ namespace Quicksorts
         }
 
         static void Main(string[] args)
-        {
+        { 
+            Console.WriteLine("Input the number of elements in the array");
+            SIZE = Convert.ToInt32(Console.ReadLine());
+
+            randArray = new int[SIZE];
+            randArray2 = new int[SIZE];
+            randArray3 = new int[SIZE];
+
             Console.WriteLine("Creating arrays");
-            if (SIZE < 10000)
-                populateArray(randArray);
-            else
-                fillArray(randArray);
+            fillArray(randArray);
             Array.Copy(randArray, randArray2, SIZE);
             Array.Copy(randArray, randArray3, SIZE);
             Console.WriteLine("Finished creating arrays");
 
+            Stopwatch stopwatch = new Stopwatch();
 
-            //insertionSort(randArray, 0, randArray.Length);
-            parallelSort(randArray);
-            Console.WriteLine("Press any key to Print array.");
+            Console.WriteLine("Insertion sort start");
+            stopwatch.Reset();
+            stopwatch.Start();
+            insertionSort(randArray, 0, randArray.Length);
+            stopwatch.Stop();
+            Console.WriteLine("Time of Insertion Sort: {0}", stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("Quicksort start");
+            stopwatch.Reset();
+            stopwatch.Start();
+            quicksort(randArray2);
+            stopwatch.Stop();
+            Console.WriteLine("Time of QuickSort: {0}", stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("Parallel sort start");
+            stopwatch.Reset();
+            stopwatch.Start();
+            parallelSort(randArray3);
+            stopwatch.Stop();
+            Console.WriteLine("Time of Parallel Sort: {0}", stopwatch.ElapsedMilliseconds);
+
+
+            Console.WriteLine("Press any key to print parallel array.");
             Console.ReadKey();
-            printArray(randArray);
+            printArray(randArray3);
+
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
